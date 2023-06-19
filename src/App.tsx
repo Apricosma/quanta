61396b24367d1128307f946edd01b7fb75725f2c
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-06-19 12:58:33 -0500

    add deps and test materialui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
-import { Index } from './components/Index'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/App.css";
+import { Index } from "./components/Index";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   
   return (
-    <>
-      <Index name="Jim" age={29}/>
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme />
+      <Index name="Jim" age={29} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,15 +1,83 @@
-import { FC } from 'react'
+import { FC } from "react";
+import {
+  Typography,
+  Box,
+  Button,
+  Chip,
+  Divider,
+  ListItem,
+  ListItemText,
+  List,
+  ListItemAvatar,
+  Avatar,
+  Paper,
+  Container,
+} from "@mui/material";
+import Icon from "@mui/material/Icon";
 
 type Props = {
-  name: string,
-  age: number,
-}
+  name: string;
+  age: number;
+};
+
+const style = {
+  width: "100%",
+  maxWidth: 360,
+  bgcolor: "background.paper",
+};
 
 export const Index: FC<Props> = ({ name, age }) => {
   return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <h2>You are {age} years old.</h2>
-    </div>
+    <Container>
+      <Box>
+        <Typography variant="h2">Hello {name}!</Typography>
+        <Typography variant="body1">You are {age} years old.</Typography>
+        <Button variant="contained">Click here!</Button>
+        <Icon>star</Icon>
+      </Box>
+      <Box>
+        <Chip
+          label="Clickable Link"
+          component="a"
+          href="#basic-chip"
+          variant="outlined"
+          clickable
+        />
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <ListItem button>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <Divider />
+          <ListItem button divider>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+          <Divider light />
+          <ListItem button>
+            <ListItemText primary="Spam" />
+          </ListItem>
+        </List>
+      </Box>
+
+      <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 128,
+          height: 256,
+        },
+      }}
+    >
+      <Paper elevation={1} />
+      <Paper elevation={2} />
+      <Paper elevation={3} />
+      <Paper elevation={4} />
+      <Paper elevation={5} />
+    </Box>
+    </Container>
   );
-}
+};
